281b6f1c31d4ca4fdd463cf488e2664d36d5d391
Matt Lenz <matt@mattlenz.com>
2016-02-24 16:55:34 +1100

    Better start script

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
-PORT=3001
 
-webpack-dev-server --content-base public/ --port 3000 --inline --hot & RENDERER_ACCESS_KEY=sekret PORT=3001 electron-render-service & open "http://0.0.0.0:3001/pdf?url=http://localhost:3000&access_key=sekret&marginsType=1&pageSize=A5"
+PRINT_PORT=3001
+WEB_PORT=3000
+ACCESS_KEY=sekret
+PAGE_SIZE=A5
+
+./node_modules/.bin/webpack-dev-server --content-base public/ --port $WEB_PORT --inline --hot & RENDERER_ACCESS_KEY=$ACCESS_KEY PORT=$PRINT_PORT ./node_modules/.bin/electron-render-service & (sleep 2 && open "http://localhost:$WEB_PORT/") & (sleep 2 && open "http://0.0.0.0:$PRINT_PORT/pdf?url=http://localhost:$WEB_PORT&access_key=$ACCESS_KEY&marginsType=1&pageSize=$PAGE_SIZE")
